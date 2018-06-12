b288b5e4b1d89146ad724be2b308f0b0029f9933
mitja <mitjanikolaus@gmail.com>
2018-06-12 10:33:28 +0200

    added checks for batch size and bidirectionality

--- models/HiddenStateAnalysisEncoderRNN.py
+++ models/HiddenStateAnalysisEncoderRNN.py
@@ -11,33 +11,31 @@ class HiddenStateAnalysisEncoderRNN(EncoderRNN):
     def forward(self, input_var, input_lengths=None):
         embedded = self.embedding(input_var)
         embedded = self.input_dropout(embedded)
-        if self.variable_lengths:
-            embedded = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths, batch_first=True)
 
-        #TODO assuming batch size of 1
-        batch_size = 1
-        input_for_rnn = embedded[0]
+        # TODO allow larger batches
+        if not input_var.size()[0] == 1: raise ValueError('Batch size must be 1')
+        batch_size = input_var.size()[0]
 
-        #TODO better init?
+        # TODO allow bidirectional RNNs
+        if self.rnn.bidirectional: raise ValueError('RNN must not be bidirectional')
         self.num_directions = 1
+
+        #init hidden state with zeros
         hidden = Variable(torch.zeros(self.num_directions, batch_size, self.hidden_size))
 
         hidden_activations_all_timesteps = []
+        full_output = torch.zeros(batch_size, input_var.size()[1], self.hidden_size)
 
-        for i in range(0,input_for_rnn.shape[0]):
-            # shape of input: seq_len, batch, input_size)
-            input = input_for_rnn[i].view(1, 1, -1)
+        for i in range(0,embedded.shape[1]):
+            input = embedded[:,i,:].view(1,1,-1) # shape of input: seq_len, batch, input_size)
 
             output, hidden = self.rnn(input, hidden)
 
+            full_output[:, i, :] = output
             hidden_activations_all_timesteps.append(hidden)
 
         ret_dict = dict()
         ret_dict[HiddenStateAnalysisEncoderRNN.KEY_HIDDEN_ACTIVATIONS_ALL_TIMESTEPS] = hidden_activations_all_timesteps
 
-        #TODO uncommented because of error
-        #if self.variable_lengths:
-        #    output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
-
-        return output, hidden, ret_dict
+        return full_output, hidden, ret_dict
 
