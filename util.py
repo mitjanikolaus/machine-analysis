029d94983c6d3e291592a8d37b492ffd0c14689c
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-06-13 18:53:46 +0200

    Added way to visualize difference in activation values over time.

--- util.py
+++ util.py
@@ -5,6 +5,7 @@ Define different utility functions, e.g. for different kinds of visualization.
 # EXT
 import torch
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import AxesGrid
 import numpy as np
 
 # PROJECT
@@ -30,7 +31,7 @@ def plot_hidden_activations(activations, input_length, num_units_to_plot=50):
     plt.show()
 
 
-def plot_activation_distributions(all_timesteps_activations: list, grid_size=None):
+def plot_activation_distributions(all_timesteps_activations: list, grid_size=None, show_title=True):
     num_timesteps = len(all_timesteps_activations)
 
     assert all([type(out) == torch.Tensor for out in all_timesteps_activations]), \
@@ -76,25 +77,64 @@ def plot_activation_distributions(all_timesteps_activations: list, grid_size=Non
     for t, (axis, activations, color) in enumerate(zip(axes, all_timesteps_activations, colors)):
         activations = activations.cpu().numpy()
         activations = _squeeze_out(activations)
-        print(activations)
         num_bins = int(len(activations) / 4)
         axis.hist(activations, num_bins, density=True, facecolor=color, alpha=0.75)
         axis.set_xlabel("t={}".format(t))
 
     fig.tight_layout()
-    fig.suptitle("Distribution of activation values over {} time steps".format(num_timesteps))
+
+    if show_title:
+        fig.suptitle("Distribution of activation values over {} time steps".format(num_timesteps))
 
     plt.subplots_adjust(top=0.85)
     plt.show()
 
 
+def plot_activation_gradients(all_timesteps_activations, neuron_heatmap_size, show_title=True):
+    num_timesteps = len(all_timesteps_activations)
+
+    assert all([type(out) == torch.Tensor for out in all_timesteps_activations]), \
+        "This function only takes all the activations for all the time steps of a single sample."
+    assert neuron_heatmap_size[0] * neuron_heatmap_size[1] >= np.multiply.reduce(all_timesteps_activations[0].size()), \
+        "Specified grid doesn't provide enough space for all plots."
+
+    fig = plt.figure()
+    last_activations = all_timesteps_activations[0]
+
+    grid = AxesGrid(
+        fig, 111, nrows_ncols=(1, num_timesteps-1), axes_pad=0.05, share_all=True, label_mode="L",
+        cbar_location="right", cbar_mode="single",
+    )
+
+    for t, (axis, current_activations) in enumerate(zip(grid, all_timesteps_activations[1:])):
+        activation_gradients = current_activations - last_activations
+        heatmap = axis.imshow(activation_gradients.reshape(*neuron_heatmap_size), cmap='coolwarm')
+        axis.set_xlabel("t={} -> t={}".format(t, t+1))
+        axis.set_xticks([])
+        axis.set_yticks([])
+
+        last_activations = current_activations
+
+    grid.cbar_axes[0].colorbar(heatmap)
+
+    if show_title:
+        fig.suptitle("Activation value gradients over {} time steps".format(num_timesteps))
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    test_data_path = './test_activations_gru_1_heldout_tables.pt'
+    test_data_path = './test_activations_lstm_1_heldout_tables.pt'
     data = ActivationsDataset.load(test_data_path)
 
     # Plot activations as heat map
     #encoder_input_length = data.model_inputs[0].shape[1]-1
     #plot_hidden_activations(data.encoder_activations[0], encoder_input_length, num_units_to_plot=50)
 
+    SAMPLE = data.hidden_activations_encoder[12]
+
     # Plot distribution of activation values in a series of time steps
-    plot_activation_distributions(data.hidden_activations_encoder[12])
+    #plot_activation_distributions(SAMPLE)
+
+    # Plot changes in activations values
+    plot_activation_gradients(SAMPLE, neuron_heatmap_size=(16, 32))
