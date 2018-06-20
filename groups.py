97182d645cf683b4e6d6f1097e4d2d814e0e05b0
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-06-20 11:48:50 +0200

    Using shapley values, balancing data set.

--- groups.py
+++ groups.py
@@ -5,22 +5,15 @@ Train a diagnostic classifier to predict the existence of input sequence element
 # STD
 import random
 import math
-from collections import Counter
+import warnings
 
 # EXT
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torch.utils.data.sampler import SubsetRandomSampler
-import torch.optim as optim
-from scipy.stats import pearsonr
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+import shap
 
 # PROJECT
 from activations import ActivationsDataset
-from visualization import plot_multiple_model_weights
 
 
 def _split(length: int, ratio=(0.9, 0.1)):
@@ -68,9 +61,9 @@ class FunctionalGroupsDataset(ActivationsDataset):
         if not self.target_feature_label_added:
             # Column describing whether the target feature is present in input sequence
             self.columns = [target_activations, "target_feature_present"]
+            num_positive = 0
 
-            regressor_decoder_hidden_states = []
-            regressor_presence_column = []
+            positive_decoder_hidden_states, negative_decoder_hidden_states = [], []
 
             for model_input, decoder_hidden_states in zip(self.model_inputs, getattr(self, target_activations)):
                 occurrence_indices = np.where(model_input.flatten() == target_feature)[0]
@@ -78,42 +71,71 @@ class FunctionalGroupsDataset(ActivationsDataset):
                 for ts, decoder_hidden_state in enumerate(decoder_hidden_states):
                     # Target feature in input: label 1
                     if ts in occurrence_indices:
-                        class_label = 1
+                        num_positive += 1
+                        positive_decoder_hidden_states.append(decoder_hidden_state)
                     else:
-                        class_label = 0
+                        negative_decoder_hidden_states.append(decoder_hidden_state)
 
-                    regressor_decoder_hidden_states.append(decoder_hidden_state)
-                    regressor_presence_column.append(class_label)
+            # Balance data set
+            regressor_decoder_hidden_states = positive_decoder_hidden_states + negative_decoder_hidden_states[:num_positive]
+            regressor_class_labels = [1] * num_positive + [0] * num_positive
 
             # Overwrite data using the new class label column
             self.regressor_decoder_hidden_states = np.array(regressor_decoder_hidden_states)
-            self.regressor_presence_column = np.array(regressor_presence_column)
+            self.regressor_presence_column = np.array(regressor_class_labels)
+
+            self.length = 2 * num_positive
 
             self.target_feature_label_added = True  # Only allow this logic to be called once
+
+
+def get_neuron_shapley_values(regressor, X_train, X_test, k=50, nsamples=100):
+    summary = shap.kmeans(X_train, k=k)
+    explainer = shap.KernelExplainer(regressor.predict_proba, summary)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        shapley_values = explainer.shap_values(X_test, nsamples=nsamples)
+        shapley_values = shapley_values[0]
+        shapley_values = shapley_values[:, :X_test.shape[1]]
+
+    return shapley_values
 
 
 if __name__ == "__main__":
     num_models = 10
     target_feature = 3  # t1 = 3
+    shapley_values = None
 
     # Load data and split into sets
     full_dataset = FunctionalGroupsDataset.load("./guided_gru_1_train.pt", convert_to_numpy=True)
-
     full_dataset.add_dataset_for_regressor(
         target_feature=target_feature, target_activations="hidden_activations_decoder"
     )
 
     for i in range(num_models):
-        #create a new random split for each model
+        # create a new random split for each model
         training_indices, test_indices = _split(len(full_dataset), ratio=(0.9, 0.1))
 
-        X = full_dataset.regressor_decoder_hidden_states
-        y = full_dataset.regressor_presence_column
+        X_train = full_dataset.regressor_decoder_hidden_states[training_indices]
+        y_train = full_dataset.regressor_presence_column[training_indices]
+        X_test = full_dataset.regressor_decoder_hidden_states[test_indices]
+        y_test = full_dataset.regressor_presence_column[test_indices]
 
         regressor = LogisticRegression()
-        regressor.fit(X[training_indices], y[training_indices])
-        print('Accuracy:', regressor.score(X[test_indices], y[test_indices]))
+        regressor.fit(X_train, y_train)
+        print('Accuracy:', regressor.score(X_test, y_test))
 
+        # Collect shapley values
+        current_shapley_values = get_neuron_shapley_values(regressor, X_train, X_test)
+        shapley_values = current_shapley_values if shapley_values is None else \
+            np.concatenate((shapley_values, current_shapley_values), axis=0)
 
+    # Summarize
+    summarized_shapley_values = shapley_values.sum(axis=0)
+    summarized_shapley_values /= summarized_shapley_values.sum(axis=0)
+    contributing_neurons = np.where[summarized_shapley_values > summarized_shapley_values.mean() + 2 * summarized_shapley_values.std()][0]
+    print(contributing_neurons)
+    a = 3
 
 
