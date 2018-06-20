041aa8a7e0af2685269727260d9856aa202f551c
mitja <mitjanikolaus@gmail.com>
2018-06-20 13:37:12 +0200

    ablation study giving similar results

--- groups.py
+++ groups.py
@@ -61,9 +61,9 @@ class FunctionalGroupsDataset(ActivationsDataset):
         if not self.target_feature_label_added:
             # Column describing whether the target feature is present in input sequence
             self.columns = [target_activations, "target_feature_present"]
+            num_positive = 0
 
-            regressor_decoder_hidden_states = []
-            regressor_presence_column = []
+            positive_decoder_hidden_states, negative_decoder_hidden_states = [], []
 
             for model_input, decoder_hidden_states in zip(self.model_inputs, getattr(self, target_activations)):
                 occurrence_indices = np.where(model_input.flatten() == target_feature)[0]
@@ -71,19 +71,23 @@ class FunctionalGroupsDataset(ActivationsDataset):
                 for ts, decoder_hidden_state in enumerate(decoder_hidden_states):
                     # Target feature in input: label 1
                     if ts in occurrence_indices:
-                        class_label = 1
+                        num_positive += 1
+                        positive_decoder_hidden_states.append(decoder_hidden_state)
                     else:
-                        class_label = 0
+                        negative_decoder_hidden_states.append(decoder_hidden_state)
 
-                    regressor_decoder_hidden_states.append(decoder_hidden_state)
-                    regressor_presence_column.append(class_label)
+            # Balance data set
+            regressor_decoder_hidden_states = positive_decoder_hidden_states + negative_decoder_hidden_states[
+                                                                               :num_positive]
+            regressor_class_labels = [1] * num_positive + [0] * num_positive
 
             # Overwrite data using the new class label column
             self.regressor_decoder_hidden_states = np.array(regressor_decoder_hidden_states)
-            self.regressor_presence_column = np.array(regressor_presence_column)
+            self.regressor_presence_column = np.array(regressor_class_labels)
+
+            self.length = 2 * num_positive
 
             self.target_feature_label_added = True  # Only allow this logic to be called once
-
 
 if __name__ == "__main__":
     target_feature = 3  # t1 = 3
@@ -101,13 +105,19 @@ if __name__ == "__main__":
         return regressor.score(X[test_indices], y[test_indices])
 
 
-    #make 50 runs with all units to get stable baseline
     X = full_dataset.regressor_decoder_hidden_states
     y = full_dataset.regressor_presence_column
+
+    num_runs = 30
+    # create some train/test splits for testing the units on equal conditions
+    train_test_splits = []
+    for i in range(num_runs):
+        train_test_splits.append(_split(len(X), ratio=(0.8, 0.2)))
+
+    #make runs with all units to get stable baseline
     baseline_accuracies = []
-    for i in range(50):
-        train_indices, test_indices = _split(len(X), ratio=(0.9, 0.1))
-        accuracy = train_regressor(X, y, train_indices, test_indices)
+    for i in range(num_runs):
+        accuracy = train_regressor(X, y, train_test_splits[i][0], train_test_splits[i][1])
         baseline_accuracies.append(accuracy)
 
     baseline = np.mean(baseline_accuracies)
@@ -117,15 +127,9 @@ if __name__ == "__main__":
     majority_baseline = 1 - len(np.where(y == 1)[0]) / len(y)
     print('Baseline (chance): ', majority_baseline)
 
-    #target accuracy: 95% of baseline (taking into account the majority baseline)
-    target_accuracy = baseline - 0.05*(baseline - majority_baseline)
+    #target accuracy: 95% of baseline
+    target_accuracy = 0.95*baseline
     print('Target accuracy: ', target_accuracy)
-
-    num_models = 5
-    #create some train/test splits for testing the units on equal conditions
-    train_test_splits = []
-    for i in range(num_models):
-        train_test_splits.append(_split(len(X), ratio=(0.8, 0.2)))
 
     #current subset
     subset_accuracy = 0
@@ -136,12 +140,11 @@ if __name__ == "__main__":
         for unit in range(full_dataset.regressor_decoder_hidden_states.shape[1]):
             if not unit in subset:
                 test_subset = subset + [unit]
-                #subset.append(unit)
                 X = full_dataset.regressor_decoder_hidden_states[:,test_subset].reshape(-1,len(test_subset))
                 y = full_dataset.regressor_presence_column
 
                 accuracies_model = []
-                for j in range(num_models):
+                for j in range(num_runs):
                     accuracy = train_regressor(X, y, train_test_splits[j][0], train_test_splits[j][1])
                     accuracies_model.append(accuracy)
 
@@ -150,7 +153,8 @@ if __name__ == "__main__":
         unit_accuracies.sort(key=lambda x: x[1], reverse=True)
         best = unit_accuracies.pop(0)
         subset.append(best[0])
-        print('units: ', subset, ' accuracy: ', best[1])
+        subset_accuracy = best[1]
+        print('units: ', subset, ' accuracy: ', subset_accuracy)
 
 
 
